550b7a672f0cb94eb69f9b230720ffe97717cdc8
Carlsson Khovis <63272281+FishChipos@users.noreply.github.com>
2023-04-12 20:26:22 +0700

    Delete patterns.ino

--- ledmatrix/patterns.ino
+++
@@ -1,3 +0,0 @@
-// Pattern format:
-// {{Pixel value, ... 64 times}, ... up to nth frame}
-bool pat_template[][64] = {{false, true, false, false, true, false, false, true, false, false, true, false, false, true, false, true, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, true, false, true, false, false, true, false, false, true, false, false, true, false, false, true, false}};
